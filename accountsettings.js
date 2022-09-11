33df39136b9838f046b0a4dfb6150336d75c331c
moses faye <104700694+Moselova@users.noreply.github.com>
2022-09-11 20:48:03 +0100

    mobile nav bar final

--- accountsettings.js
+++ accountsettings.js
@@ -26,5 +26,22 @@ function showPass3() {
 		z.type = "password";
 	}
 }
+function menutoogle() {
+	document.getElementById("Container").style.width = "60%";
+	document.getElementById("sidenav-2").style.width = "40%";
+	document.getElementById("sidenav-2").style.display = "block";
+}
+
+function closeNav() {
+	document.getElementById("mySidenav").style.width = "0";
+}
+
+function closeNav() {
+	document.getElementById("Container").style.width = "100%";
+	document.getElementById("mySidenav").style.display = "none";
+}
+function menutoogle() {
+	document.getElementById("mySidenav").style.display = "flex";
+}
 
 
--- contest.js
+++ contest.js
@@ -14,3 +14,20 @@ function noclose() {
 		// var nopop = document.getElementById('nopop')
 		//event.stopPropagation();
 }
+function menutoogle() {
+	document.getElementById("Container").style.width = "60%";
+	document.getElementById("sidenav-2").style.width = "40%";
+	document.getElementById("sidenav-2").style.display = "block";
+}
+
+function closeNav() {
+	document.getElementById("mySidenav").style.width = "0";
+}
+
+function closeNav() {
+	document.getElementById("Container").style.width = "100%";
+	document.getElementById("mySidenav").style.display = "none";
+}
+function menutoogle() {
+	document.getElementById("mySidenav").style.display = "flex";
+}
